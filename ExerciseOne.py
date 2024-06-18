a50b7539dae0f9edb7a3689558ff5428c8b2f03a
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-06-18 13:40:19 +0530

    Add files via upload

---
+++ ExerciseOne.py
@@ -0,0 +1,59 @@
+# Input the name from user ( Enter Name Middlename Surname)
+name = 
+
+# Print the name 
+print("Hello",______)
+
+
+# Capitalize the first letter of name 
+name = ______.capitalize()
+
+
+# Capitalize the first letter of each word
+name = ____.title()
+
+# ValueError 
+"""
+A ValueError in Python is a type of exception that occurs
+when a function receives an argument of the correct type 
+but an inappropriate value.
+"""
+first, last = _____.split()
+print(f"Hello {first}")
+
+# Integers
+x = input("What's your Number?")
+y = input("What's your Number?")
+div= _______________
+mul = _____________
+add = _____________
+sub = ____________
+
+print(div)
+print(mul)
+print(add)
+print(sub)
+
+x = int(input("What's your Number?"))
+y = int(input("What's your Number?"))
+mod = _______
+floorDiv = _______
+add = ________
+sub = _______
+print(x+y)
+print(floorDiv)
+print(mul)
+print(add)
+print(sub)
+print(int(x/y))
+
+# Find the data type
+
+print(_____(add))
+
+# Change the data type and then confirm it by checking the datatype
+addFloat = _____(add)
+print(_____(addFloat))
+
+# Check the data type
+print(_____(name))
