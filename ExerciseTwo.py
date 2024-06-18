dd8157a0c8f7528e634eb9f638452bbb5a8ca198
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-06-18 14:39:15 +0530

    Add files via upload

---
+++ ExerciseTwo.py
@@ -0,0 +1,12 @@
+import cowsay
+import pyttsx3
+voice = pyttsx3.init()
+data  =input("What's this?")
+if ________< 10:
+    cowsay._____(data)
+    voice.______(data)
+    voice.runAndWait()
+else:
+    cowsay.____(data)
+    voice.___(data)
+    voice.runAndWait()
