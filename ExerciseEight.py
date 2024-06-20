57ad7d7ecb55db06957ea8eb629495f9b90f75df
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-06-20 13:36:19 +0530

    Add files via upload

---
+++ ExerciseEight.py
@@ -0,0 +1,34 @@
+# Recursive  
+
+# Factorial:-
+
+def fact_rec(n):
+    # Base Case 
+    pass
+    # Recursive case
+    pass
+print(fact_rec(3))
+
+# GCD or LCM :-
+
+def rec_gcd(a, b):
+    # Base Case
+    pass
+    # Recusrive case
+    pass
+print(rec_gcd(14,12))
+
+
+# Fibonacci Series
+def rec_fib(n):
+
+    # Base Case 1
+    pass
+    
+    # Base Case 2
+    pass
+    
+    # Recursive case
+    pass
+
+print(rec_fib(7))
---
+++ ExerciseNine.py
@@ -0,0 +1,41 @@
+""" . any character except a new line
+    * 0 or more repetitions
+    + 1 or more repetitions
+    ? 0 or 1 repetition
+    {m} m repetitions
+    {m,n} m-n repetitions 
+    ^ Matches start of string
+    $ End of string
+    [] set of characters
+    [^] Complementing the set
+    \d decimal digit
+    \D not a decimal digit
+    \s Whitespace character
+    \S not a Whitespace character
+    \w Word character
+    \W Not a word Character
+"""
+
+# Regex 1 Check the @ function
+
+
+# Regex 2  CHeck Any character along the space  with 0 or more repetition
+
+
+# Regex 3   Check the ".com" is there or not
+
+
+# Regex 4
+
+
+# Regex 5  ".com" at the end 
+
+
+# Regex 6
+
+
+# Regex 7 
+
+
+# Regex 8 Ignore Case
+
