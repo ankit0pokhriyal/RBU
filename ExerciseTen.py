8dbe9ab7b5e3e8278561cc573a3b23600392bd25
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-06-22 11:07:08 +0530

    Add files via upload

---
+++ ExerciseTen.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self,name,job, pay):
+        pass
+ankit  = Person('Ankit Pokhriyal', 1, 3432)
+print(ankit.name, ankit.job, ankit.pay)
+
+# if __name__ == '__main__':
